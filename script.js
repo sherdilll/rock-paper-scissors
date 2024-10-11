4105abad242484b5c1bd94261ed3b04b216279fe
Divesh Kumar <104316863+sherdilll@users.noreply.github.com>
2024-10-11 15:59:25 +0530

    Implement logic to play a single round

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+humanScore = 0;
+computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log(`It's a tie! Both chose ${humanChoice}`);
+    return;
+  }
+
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+    humanScore++;
+  } else {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  }
+
+  console.log(
+    `Current score - Human: ${humanScore}, computer: ${computerScore}`
+  );
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
 function getComputerChoice() {
   let random = Math.random();
 
